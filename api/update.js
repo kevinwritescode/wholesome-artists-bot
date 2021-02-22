8b5ec74558d788c466707ad44f16bd80b852e7b9
Kevin Nuut <kevin@nuut.co>
2021-02-21 20:18:55 -0600

    clean up test call

--- api/update.js
+++ api/update.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 module.exports = (req, res) => {
   client.once('ready', async () => {
-    const channel = await client.channels.cache.fetch('744976954913980577');
+    const channel = await client.channels.cache.get('744976954913980577');
     const messages = channel.messages.fetch({ limit: 100 });
   
     channel.send('Kevin is testing, there are ' + messages.length + ' new messages');
