c0965c2a20010702eef7f20bad7e1b4181bfd74b
Kevin Nuut <kevin@nuut.co>
2021-02-21 21:23:56 -0600

    finish local testing and mask token

--- api/update.js
+++ api/update.js
@@ -1,14 +1,29 @@
 const Discord = require('discord.js');
+
+const CHANNEL_MODS = '769028512660979743';
+const CHANNEL_PROMOTE = '740423263683084341';
+
 const client = new Discord.Client();
 
 module.exports = (req, res) => {
   client.once('ready', async () => {
-    const channel = await client.channels.cache.get('744976954913980577');
-    const messages = await channel.messages.fetch({ limit: 100 });
+    const promoteChannel = await client.channels.cache.get(CHANNEL_PROMOTE);
+    const modsChannel = await client.channels.cache.get(CHANNEL_MODS);
+    const messages = await promoteChannel.messages.fetch({ limit: 100 });
   
     const latest = messages.values().next().value;
-    const output = `Kevin is testing, there are ${messages.size} new messages and last message was "${latest.content}" by <@${latest.author.id}>`;
-    await channel.send(output);
+
+    let output;
+    if (latest?.embeds?.[0]) {
+      const embed = latest.embeds[0];
+      // A hack to get the authorId from their icon since this embeded post doesn't natively share it
+      const authorId = embed.author.iconURL.split('/')[4];
+      output = `<@${authorId}> posted ${embed.image.url}`;
+    } else {
+      output = `<@${latest.author.id}> said "${latest.content}"`;
+    }
+
+    await modsChannel.send(output);
 
     res.json({
       message: output,
--- tests/update.js
+++ tests/update.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const apiUpdate = require('../api/update');
-process.env.DISCORD_TOKEN = 'NzU1OTQ4ODg2MDEwMzYzOTQ2.X2KuQw.ZWehDHkppcMsSb_GF5I-aF4hRp8';
 
 describe('Update', () => {
   it('success', () => {
